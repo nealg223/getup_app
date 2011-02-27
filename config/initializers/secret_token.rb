23370bea664c54f75b0dc630ba5cdd59f8703169
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-02-26 19:46:24 -0600

    thanks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetupApp::Application.config.secret_token = '6a6b853672e566425554cc6e5112f64f255d6ddc608d5febab0e305447603b1f6b2e3b6502d34cd9a71aac6c9b08c8a9d92a96bb70ed484d0b4680ff4a2c2d24'
