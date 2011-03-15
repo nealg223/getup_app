6efaee8960bf6944492abe9f4fee1e4dc4530573
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-03-14 22:54:07 -0500

    got started on the user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def logo
+    # Fill in
+    # This will entail using the code <% logo = image_tag("logo.png", :alt => "Getup!", :class => "round") %> to some capacity!
+  end
+  
+  # Return a title on a per page basis
+  def title
+    
+    
+    
+  end
 end
